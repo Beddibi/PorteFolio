01b7892a9b6831c371ffa4fd9a208209f3827127
Mohammed Beddibi <sbedibi.mohamed@gmail.com>
2024-12-25 11:19:23 +0100

    add config git

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes, withHashLocation } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { ApplicationConfig } from '@angular/core';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -15,4 +16,8 @@ export const routes: Routes = [
     { path: 'contact', component: ContactComponent },
     { path: '**', redirectTo: '' }
   ];
-  
+  export const appConfig: ApplicationConfig = {
+    providers: [
+      provideRouter(routes, withHashLocation())
+    ]
+  };
